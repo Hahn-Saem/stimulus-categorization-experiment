2044435b43fdf24512664c663eb34ce5b4e08511
Hahn-Saem <hl120@illinois.edu>
2026-04-03 10:43:11 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    window_height = 800
+    window_width = 1200
+
+    instructions_bg_color = "white"
+    instructions_font_color = "black"
+    instructions_font_size = 18
+    instructions_font = "helvetica"
+    instruction_delay = 1000 
+
+    stimulus_bg_color = "white"
+    image_stimulus_height = 400
+    image_stimulus_width = 400
+
+    test_key_list = ["f", "j"]
+
+    instruction_file_path_list = [] # might not need because it's in the same directory
